b9b000072fef11269693b7756ba1a1f9ced2cfc9
merinjo90 <merinjo90@gmail.com>
2021-11-13 05:33:15 -0500

    Create Notes section complted   -form created ,data will save the data base   -method='POST'; then added csrf_token

--- studentPortal/studentStudyPortal/dashboard/views.py
+++ studentPortal/studentStudyPortal/dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from . forms import *
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -9,7 +11,14 @@ def home(request):
 
 #Student Notes
 def notes(request):
-    form =NotesForm()
+    if request.method== "POST":
+        form=NotesForm(request.POST)
+        if form.is_valid():
+            notes =Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request, f"Notes Added form {request.user.username} Successfully")
+    else:
+        form =NotesForm()
     notes=Notes.objects.filter(user=request.user)
     context={'notes':notes,'form':form}
     return render(request,'dashboard/notes.html',context)
