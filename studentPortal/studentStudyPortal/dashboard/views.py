be56cfcce283cc5daff87f954e2402e256ed7c74
merinjo90 <merinjo90@gmail.com>
2021-11-13 03:08:44 -0500

    Note section added, add notes template file and connection data base connection created 1. python manage.py makemigrations 2. python manage.py migrate 3. python manage.py createsuperuser

--- studentPortal/studentStudyPortal/dashboard/views.py
+++ studentPortal/studentStudyPortal/dashboard/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#home Page
 def home(request):
     return render(request,'dashboard/home.html')
+
+#Student Notes
+def notes(request):
+    return render(request,'dashboard/notes.html')
