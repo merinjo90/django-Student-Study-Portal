f72ffda10d98d7eb47085bfc18cf64c31ea8f2ae
merinjo90 <merinjo90@gmail.com>
2021-11-19 02:57:25 -0500

    profile : todo, homework check finished or not

--- studentPortal/studentStudyPortal/dashboard/views.py
+++ studentPortal/studentStudyPortal/dashboard/views.py
@@ -336,4 +336,22 @@ def register(request):
 
 #Profile Page
 def profile(request):
-    return render(request,'dashboard/profile.html')
+    homeworks=Homework.objects.filter(is_finished=False,user=request.user)
+    todos=Todo.objects.filter(is_finished=False,user=request.user)
+    if len(homeworks)==0:
+        homework_done=True
+    else:
+        homework_done=False
+
+    if len(todos)==0:
+        todos_done=True
+    else:
+        todos_done=False
+    context={
+        'homeworks':homeworks,
+        'todos':todos,
+        'homework_done':homework_done,
+        'todos_done':todos_done
+    }
+
+    return render(request,'dashboard/profile.html',context)
